821c3ed1f0eaaedc8bc714477c70d38e592d1c0f
dapodkovko <dapodkovko@gmail.com>
2020-10-25 23:48:42 +0200

    created model

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Project, Rating
 
-# Register your models here.
+
+class RatingInline(admin.TabularInline):
+    model = Rating
+    extra = 1
+    verbose_name_plural = 'Ratings'
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [RatingInline]
+    list_display = ['id', 'project_description']
+    list_filter = ['project_description']
+    search_fields = ['project_description']
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'project', 'user', 'stars']
+    list_filter = ['project', 'user', 'stars']
+    raw_id_fields = ['project']
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Rating, RatingAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Project(models.Model):
+    project_description = models.TextField(max_length=5000, db_index=True, verbose_name='Project description')
+
+    def number_of_ratings(self):
+        ratings = Rating.objects.filter(project=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(project=self)
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
+    def __str__(self):
+        return self.project_description[:50]
+
+
+class Rating(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings', verbose_name='Project')
+    user = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, related_name='ratings',
+                             verbose_name='User')
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], default=0)
+
+    def __str__(self):
+        return 'Project "{}"'.format(self.project)
