7c4ea2e570dbb9120b4d0c9074c1e9290b1771fd
dapodkovko <dapodkovko@gmail.com>
2020-11-09 15:35:18 +0200

    login via email

--- users/models.py
+++ users/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
     avatar = models.ImageField(blank=True, null=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
     def __str__(self):
-        return "@{}".format(self.username)
+        return "@{} using {}".format(self.username, self.email)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username='user_1', email='user_1@gmail.com', password='password')
+        self.assertEqual(user.email, 'user_1@gmail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username='', email='', password="password")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser('superuser', 'superuser@gmail.com', 'password')
+        self.assertEqual(admin_user.email, 'superuser@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                username='superuser', email='superuser@gmail.com', password='password', is_superuser=False)
