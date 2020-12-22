e0593c7007cf935692bf623f9610e6250e82bc23
dapodkovko <dapodkovko@gmail.com>
2020-12-22 22:49:02 +0200

    add bootstrap/some tests

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', include('catalog.urls')),
     path('catalog/', include('catalog.urls')),
 ]
