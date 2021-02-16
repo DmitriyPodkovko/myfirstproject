c8ce89378c7afbc26ba8a733021036ebe0bc7a58
dapodkovko <dapodkovko@gmail.com>
2021-02-16 10:24:22 +0200

    fix test

--- catalog/tests.py
+++ catalog/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class HomePageTests(SimpleTestCase):
 
     def test_home_page_status_code(self):
-        response = self.client.get('/')
+        response = self.client.get('/catalog/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_url_by_name(self):
@@ -15,13 +15,13 @@ class HomePageTests(SimpleTestCase):
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
 
 class SignupPageTests(TestCase):
     username = 'newuser'
     email = 'newuser@email.com'
+    password = 'Superpassword'
 
     def test_signup_page_status_code(self):
         response = self.client.get('/users/signup/')
@@ -33,11 +33,17 @@ class SignupPageTests(TestCase):
 
     def test_view_uses_correct_template(self):
         response = self.client.get(reverse('signup'))
-        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
 
     def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(self.username, self.email)
-        self.assertEqual(get_user_model().objects.all().count(), 1)
-        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
-        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
+        response = self.client.post(reverse('signup'), {
+            'username': self.username,
+            'email': self.email,
+            'password1': self.password,
+            'password2': self.password
+        }, follow=True)
+        self.assertEqual(response.status_code, 200)
+        users = get_user_model().objects
+        self.assertEqual(users.all().count(), 1)
+        self.assertEqual(users.first().username, self.username)
+        self.assertEqual(users.first().email, self.email)
