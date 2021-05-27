83d48fd7160e346a3d08c7ff08c5b74a120e42ca
dapodkovko <dapodkovko@gmail.com>
2021-05-27 16:59:25 +0300

    add model Rating

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from forum.models import ProjectForum
 
 
@@ -18,9 +20,10 @@ class Category(models.Model):
 
 
 class Project(models.Model):
-    user = models.ForeignKey('users.CustomUser', on_delete=models.SET_NULL, null=True, related_name='projects')
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='projects')
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='projects')
     description = models.TextField(max_length=5000, db_index=True)
+    stars = models.IntegerField(default=0)
     created_at_comment = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -32,3 +35,14 @@ class Project(models.Model):
 
     def __str__(self):
         return self.description[:50]
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='ratings')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings')
+    score = models.IntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.score
