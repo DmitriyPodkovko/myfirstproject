5fba1f514bf4bfb2d05f30fcd3c87554be42e644
DmitriyPodkovko <36520871+DmitriyPodkovko@users.noreply.github.com>
2021-08-10 13:45:35 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -123,14 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = BASE_DIR / 'static'
-else:
-   STATIC_ROOT = BASE_DIR / 'static'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
